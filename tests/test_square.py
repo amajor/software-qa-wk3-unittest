ba83aeff6e3f05838ee006e9e364e5173fb80a6b
Alison Major <alison.major@gmail.com>
2021-04-03 22:58:22 -0400

    Replace square tests with mocks

--- tests/test_square.py
+++ tests/test_square.py
@@ -1,44 +1,29 @@
 import unittest
 from app.square import Square
-from app.math import Math
-from parameterized import parameterized
+from unittest.mock import MagicMock
 
 
 class TestSquare(unittest.TestCase):
     def setUp(self):
-        side = 2
-        math_util = Math()
-        self._square = Square(side, math_util)
+        # Set up mocks
+        self._math_mock = MagicMock()
+        self._math_mock.power = MagicMock(return_value=25)
+        self._math_mock.add = MagicMock(return_value=20)
+
+        # Set up Square()
+        default_side = 5
+        self._square = Square(default_side, self._math_mock)
 
     def test_area(self):
-        expected = 4
+        expected = 25
         result = self._square.area()
         self.assertEqual(expected, result)
 
     def test_circumference(self):
-        expected = 8
+        expected = 20
         result = self._square.circumference()
         self.assertEqual(expected, result)
 
-    @parameterized.expand([
-        ("side = 0", Square(0, Math()), 0),
-        ("side = 1", Square(1, Math()), 1),
-        ("side = 99", Square(99, Math()), 9801),
-    ])
-    def test_area_parameterized(self, name, square, expected):
-        self._square = square
-        result = self._square.area()
-        self.assertEqual(expected, result)
-
-    @parameterized.expand([
-        ("side = 0", Square(0, Math()), 0),
-        ("side = 1", Square(1, Math()), 4),
-        ("side = 99", Square(99, Math()), 396),
-    ])
-    def test_circumference_parameterized(self, name, square, expected):
-        self._square = square
-        result = self._square.circumference()
-        self.assertEqual(expected, result)
 
 if __name__ == '__main__':
     unittest.main()
